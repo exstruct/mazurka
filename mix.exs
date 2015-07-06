00fbad538051428655b11cb3cda92a4941329820
camshaft <bytheway.cameron@gmail.com>
2015-07-06 11:06:26 -0600

    release 0.2.2

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Mazurka.Mixfile do
 
   def project do
     [app: :mazurka,
-     version: "0.2.1",
+     version: "0.2.2",
      elixir: "~> 1.0",
      description: "hypermedia api toolkit",
      package: package,
