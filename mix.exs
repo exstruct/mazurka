e6e03c68f8a43abbca95532816198e9349eab656
Cameron Bytheway <bytheway.cameron@gmail.com>
2016-06-08 15:20:01 -0600

    release 0.3.37

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Mazurka.Mixfile do
 
   def project do
     [app: :mazurka,
-     version: "0.3.36",
+     version: "0.3.37",
      elixir: "~> 1.0",
      description: "hypermedia api toolkit",
      package: package,
